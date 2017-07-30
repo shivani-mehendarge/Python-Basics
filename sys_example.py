65973eec26304734e4c2e9f192bf513b8df209f5
shivani <smshivani579@gmail.com>
2017-07-30 14:20:12 +0530

    Todo list modified

--- sys_example.py
+++ sys_example.py
@@ -3,24 +3,37 @@ import os
 import pdb
 #pdb.set_trace() #for debugging
 if len(sys.argv) ==  1:
-    print("Error. Not sufficient arguments")
+    print("Error. Not sufficient arguments\n Usage: \n 1. To append to a file : python3 sys_example.py -a \"string to add\" \n 2. To display the file: python3 sys_example.py -v")
     sys.exit(1)
 
 if len(sys.argv) >= 1:
     file_name = "task.txt"
     option = sys.argv[1]
     try:
+	#append the text to the file task.txt
         if option == '-a':
-            with open(file_name,'a') as f:
-                f.write(sys.argv[2]+"\n")
+            if not os.path.isfile(file_name):
+                with open(file_name,'w') as f:
+                    f.write(sys.argv[2]+"\n")
+            else:
+                with open(file_name,'a') as f:
+                    f.write(sys.argv[2]+"\n")
             print("Added to the file\n")
+
+	#display the contents of the file
         elif option == '-v':
-            with open(file_name,'r') as f:
-                for line in f:
-                    print(line)
+            if not file_name:
+                print("Nothing to display!")
+                sys.exit(1)
+            else:
+                with open(file_name,'r') as f:
+                    print(f.read())
             print("File END!!")
+	
+	#delete the contents of the file
         elif option == '-d':
             pass
+
         else:
             print("invalid arguments")
 
